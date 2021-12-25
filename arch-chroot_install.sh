c433ce7651bbfb886854bacc0c9d4a836b27f7b0
tv-funny <76856769+tv-funny@users.noreply.github.com>
2021-12-25 10:47:05 +0800

    Add files via upload

---
+++ arch-chroot_install.sh
@@ -0,0 +1,18 @@
+#/bin/bash
+ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
+hwclock --systohc
+echo 'en_US.UTF-8 UTF-8' >> /etc/locale.gen
+locale-gen
+echo 'LANG=en_US.UTF-8' >> /etc/locale.conf
+echo 'arch' >> /etc/hostname
+echo '
+127.0.0.1localhost
+::1		localhost
+127.0.1.1	arch.localdomain	arch' >> /etc/hosts
+echo 'enter your root password'
+passwd
+pacman -S intel-ucode grub efibootmgr
+mkdir /boot/efi
+grub-install --efi-directory=/boot/efi
+grub-mkconfig -o /boot/grub/grub.cfg
+systemctl enable dhcpcd
